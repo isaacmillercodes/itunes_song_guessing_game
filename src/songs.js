84fba99740835c9cc045a2e537eeb174276584de
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2016-07-20 16:14:51 -0600

    got the song title guesser will add artist name and album name

--- src/songs.js
+++ src/songs.js
@@ -1,12 +1,45 @@
 $(function() {
 
- var songIds = ["995535015", "966411602", "823593456", "956689796", "943946671",
-                 "982388023", "907242704", "201281527", "656801339", "910038357",
-                 "250038575", "878000348",  "794095205",  "1645339",  "400835962",
-                 "325618",  "169003415",  "51958108",
-                 "192688540", "684811768", "344799464", "217633921",
-                 "192811017", "71068886", "640047583", "517438248",
-                "656479859", "310237", "991390352", "901614155",
-                 "344799727", "162337613", "121695005", "159293848", "305118379" ];
+  //get random song ID
+  var randomSongID = getRandomSongID(songIds);
+
+  //get random song file
+  getItunesData(randomSongID);
+
+  //append random song title to DOM
+  function getItunesData(songID) {
+
+    $.ajax({
+      url: 'https://itunes.apple.com/lookup?id=' + songID,
+      dataType: 'jsonp'
+    }).done(function(data) {
+      var musicObject = data.results[0];
+      // var musicURI = data.results[0].previewUrl;
+      var html = '<audio id="audio_preview" src="' + musicObject.previewUrl + '" autoplay></audio>'
+      $('body').append(html);
+      $('#answer').attr('value', musicObject.trackName);
+    });
+  }
+
+  //get form input
+
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+    var songTitle = $('#song-title').val();
+    var songTitleAnswer = $('#answer').val();
+    console.log(songTitleAnswer);
+    if (songTitle === songTitleAnswer) {
+      $('#response').text('Nailed it!')
+    } else {
+      $('#response').text("Welp, you suck.")
+    }
+  })
+
 });
 
+//helpers
+
+function getRandomSongID(songArray) {
+  var randomID = songArray[Math.floor(Math.random()*(songArray.length))];
+  return randomID;
+}
